c7cf410823070be362db09ef88dd59240ec497de
AmitfFishel <154337319+AmitfFishel@users.noreply.github.com>
2024-01-01 15:07:47 +0200

    Solved.

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -10,21 +10,62 @@ import java.util.Random;
 public class OneOfEachStats {
 	public static void main (String[] args) {
 		// Gets the two command-line arguments
-		int T = Integer.parseInt(args[0]);
+		int numbersOfFamlies = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed); 
 		double rnd = generator.nextDouble(); 
+		boolean chanceGirl = (rnd >= 0 && rnd < 0.5);
+		boolean chanceBoy = (rnd >= 0.5 && rnd < 1);
+		int sum = 0;
+
+		double average = 0;
+		int twoKids = 0;
+		int threeKids = 0;
+		int fourkidsOrMore = 0;
+
+       for(int i = 1; i <= numbersOfFamlies; i++) {
+
+		if (chanceGirl==true) {
+			while(chanceGirl==true) {
+				sum++;
+				rnd = generator.nextDouble();
+				chanceGirl = (rnd >= 0 && rnd < 0.5);
+	         }
+
+			 sum++; // יצאנו מהלולאה כי קיבלנו בן ולכן הוספנו אותו לסכום.
+			
+			} else { 
+				while(chanceBoy==true) {
+				sum++;
+				rnd = generator.nextDouble();
+				chanceBoy = (rnd >= 0.5 && rnd < 1);
+				}
+				sum++;
+			
+			}
+            
+			average += sum;  // לסכום את כל הילדים מכל המשפחות
+			if(sum == 2) twoKids++;
+			if(sum == 3) threeKids++;
+			if(sum >=4) fourkidsOrMore++;
+			rnd = generator.nextDouble();
+			 chanceGirl = (rnd >= 0 && rnd < 0.5);
+		     chanceBoy = (rnd >= 0.5 && rnd < 1);
+			sum = 0; 
+
+		}
+
+			System.out.println("Average: " + (average / numbersOfFamlies) + " children to get at least one of each gender.");
+			System.out.println("Number of families with 2 children: " + twoKids);
+			System.out.println("Number of families with 3 children " + threeKids);
+			System.out.println("Number of families with 4 or more children: " + fourkidsOrMore);
+			int max = (Math.max(Math.max(twoKids, threeKids), fourkidsOrMore));
+			if (max == twoKids) System.out.println("The most common number of children is 2.");
+			if (max == threeKids) System.out.println("The most common number of children is 3.");
+			if (max == fourkidsOrMore) System.out.println("The most common number of children is 4 or more.");
 		
-		//// In the previous version of this program, you used a statement like:
-		//// double rnd = Math.random();
-		//// Where "rnd" is the variable that stores the generated random value.
-		//// In this version of the program, replace this statement with:
-		//// double rnd = generator.nextDouble();
-		//// This statement will generate a random value in the range [0,1),
-		//// just like you had in the previous version, except that the 
-		//// randomization will be based on the given seed.
-		//// This is the only change that you have to do in the program.
-		    
+	   }
+
 	}
-}
+
